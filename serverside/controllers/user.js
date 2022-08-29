dff8991efdf83f76b85e77a58d7cd2f705432122
naseeb-shah <102802675+naseeb-shah@users.noreply.github.com>
2022-08-29 20:32:20 +0530

    addome

---
+++ serverside/controllers/user.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+
+const bcrypt = require('bcrypt')
+ const crypto =require('crypto');
+
+const main = require('./emal.send');
+
+async function register(req, res) {
+  
+}
+
+
+
+async function login(req, res) {
+
+
+}
+
+
+
+
+async function loged(req, res) {
+
+}
+
+module.exports = {
+   login, register, loged
+}
---
+++ serverside/index.js
@@ -0,0 +1,29 @@
+const express =require('express')
+var jwt = require('jsonwebtoken');
+var body= require('body-parser');
+const app=express()
+const  {register,login,loged}= require("./controllers/user")
+ const bcrypt =require('bcrypt')
+const { decode } = require('punycode');
+const exp = require('constants');
+const { application } = require('express');
+const bodyParser = require('body-parser');
+const dataconnect = require('./database/index');
+// app.use(express.json())
+app.use(bodyParser.json())
+app.get('/loged',loged)
+
+ app.post('/reg',register)
+ app.post('/login',login)
+
+
+
+
+   app.listen(4000,(err,res)=>{
+      if(err)
+      console.log(err)
+      else
+      console.log(`Server is Started at 4000`)
+   })
+
+
